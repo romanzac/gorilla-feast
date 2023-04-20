8dd639aa18c60a1259326d69224bd572c0cc7b33
Roman <zajic@zajic.net>
2023-04-20 16:57:33 +0800

    Timeout increase

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build gorilla-feast for Linux AMD64
+# Build gorilla-feast for Linux AMD64 with love
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o gorilla-feast-linux
 
 docker build --no-cache -t "gorilla-feast:1.0.2" -f Dockerfile .
